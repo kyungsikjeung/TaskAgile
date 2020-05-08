bc07579b3066f0f0fa256fb153164429ab47be53
kyungsikjeung <datamining7830@gmail.com>
2020-05-08 11:51:08 +0900

    register model, method 추가

--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -6,18 +6,33 @@
           <img class="logo" src="/static/images/logo.png" />
           <div class="tagline">Open source task management tool</div>
         </div>
-        <form>
+        <form @submit.prevent="submitForm">
           <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" />
+            <input
+              type="text"
+              class="form-control"
+              id="username"
+              v-model="form.username"
+            />
           </div>
           <div class="form-group">
             <label for="emailAddress">Email address</label>
-            <input type="email" class="form-control" id="emailAddress" />
+            <input
+              type="email"
+              class="form-control"
+              id="emailAddress"
+              v-model="form.emailAddress"
+            />
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" />
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="form.password"
+            />
           </div>
           <button type="submit" class="btn btn-primary btn-block">
             Create account
@@ -49,6 +64,20 @@
     </footer>
   </div>
 </template>
+<script>
+export default {
+  name: 'RegisterPage',
+  data: function () {
+    return {
+      form: { username: '', emailAddress: '', password: '' }
+    }
+  },
+  methods: {
+    submitForm () {
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .container {
