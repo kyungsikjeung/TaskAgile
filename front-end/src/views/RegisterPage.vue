ec8073620bd0e4e4c1089c480fea0dd8e6a9f111
kyungsikjeung <datamining7830@gmail.com>
2020-05-11 10:29:23 +0900

    로고 추가. e2e 제거. RegisterPage.vue

--- front-end/src/views/RegisterPage.vue
+++ front-end/src/views/RegisterPage.vue
@@ -3,90 +3,59 @@
     <div class="row justify-content-center">
       <div class="register-form">
         <div class="logo-wrapper">
-          <img class="logo" src="/static/images/logo.png">
+          <img class="logo" src="/static/images/logo.png" />
           <div class="tagline">Open source task management tool</div>
         </div>
-        <form @submit.prevent="submitForm">
-          <div v-show="errorMessage" class="alert alert-danger failed">{{ errorMessage }}</div>
+        <form>
           <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" v-model="form.username">
-            <div class="field-error" v-if="$v.form.username.$dirty">
-              <div class="error" v-if="!$v.form.username.required">Username is required</div>
-              <div class="error" v-if="!$v.form.username.alphaNum">Username can only contain letters and numbers</div>
-              <div class="error" v-if="!$v.form.username.minLength">Username must have at least {{$v.form.username.$params.minLength.min}} letters.</div>
-              <div class="error" v-if="!$v.form.username.maxLength">Username is too long. It can contains maximium {{$v.form.username.$params.maxLength.max}} letters.</div>
-            </div>
+            <input type="text" class="form-control" id="username" />
           </div>
           <div class="form-group">
             <label for="emailAddress">Email address</label>
-            <input type="email" class="form-control" id="emailAddress" v-model="form.emailAddress">
-            <div class="field-error" v-if="$v.form.emailAddress.$dirty">
-              <div class="error" v-if="!$v.form.emailAddress.required">Email address is required</div>
-              <div class="error" v-if="!$v.form.emailAddress.email">This is not a valid email address</div>
-              <div class="error" v-if="!$v.form.emailAddress.maxLength">Email address is too long. It can contains maximium {{$v.form.emailAddress.$params.maxLength.max}} letters.</div>
-            </div>
+            <input type="email" class="form-control" id="emailAddress" />
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" v-model="form.password">
-            <div class="field-error" v-if="$v.form.password.$dirty">
-              <div class="error" v-if="!$v.form.password.required">Password is required</div>
-              <div class="error" v-if="!$v.form.password.minLength">Password is too short. It can contains at least {{$v.form.password.$params.minLength.min}} letters.</div>
-              <div class="error" v-if="!$v.form.password.maxLength">Password is too long. It can contains maximium {{$v.form.password.$params.maxLength.max}} letters.</div>
-            </div>
+            <input type="password" class="form-control" id="password" />
           </div>
           <button type="submit" class="btn btn-primary btn-block">Create account</button>
-          <p class="accept-terms text-muted">By clicking “Create account”, you agree to our <a href="#">terms of service</a> and <a href="#">privacy policy</a>.</p>
-          <p class="text-center text-muted">Already have an account? <a href="/login">Sign in</a></p>
+          <p class="accept-terms text-muted">
+            By clicking “Create account”, you agree to our
+            <a href="#">terms of service</a> and
+            <a href="#">privacy policy</a>.
+          </p>
+          <p class="text-center text-muted">
+            Already have an account?
+            <a href="/login">Sign in</a>
+          </p>
         </form>
       </div>
     </div>
     <footer class="footer">
       <span class="copyright">&copy; 2018 TaskAgile.com</span>
       <ul class="footer-links list-inline float-right">
-        <li class="list-inline-item"><a href="#">About</a></li>
-        <li class="list-inline-item"><a href="#">Terms of Service</a></li>
-        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-        <li class="list-inline-item"><a href="https://github.com/taskagile/vuejs.spring-boot.mysql" target="_blank">GitHub</a></li>
+        <li class="list-inline-item">
+          <a href="#">About</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">Terms of Service</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="#">Privacy Policy</a>
+        </li>
+        <li class="list-inline-item">
+          <a href="https://github.com/taskagile/vuejs.spring-boot.mysql" target="_blank">GitHub</a>
+        </li>
       </ul>
     </footer>
   </div>
 </template>
 
 <script>
-import { required, maxLength, minLength, email, alphaNum } from 'vuelidate/lib/validators'
-import registrationService from '@/services/registration'
-
 export default {
-  name: 'RegisterPage',
-  data: function () {
-    return {
-      form: { username: '', emailAddress: '', password: '' }
-    }
-  },
-  validations: {
-    form: {
-      username: { required, minLength: minLength(2), maxLength: maxLength(50), alphaNum },
-      emailAddress: { required, minLength: minLength(5), maxLength: maxLength(100), email },
-      password: { required, minLength: minLength(5), maxLength: maxLength(30) }
-    }
-  },
-  methods: {
-    submitForm () {
-      this.$v.$touch()
-      if (this.$v.$invalid) {
-
-      }
-
-      registrationService.register(this.form).then(() => {
-        this.$router.push({ name: 'LoginPage' })
-      }).catch((error) => {
-        this.errorMessage = 'Failed to register user. ' + error.message
-      })
-    }
-  }
-}
+  name: "RegisterPage"
+};
 </script>
 
 <style lang="scss" scoped>
